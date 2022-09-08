d147aea33ad0469ba0304741d53febe3f0cce1b5
scarfboy <scarfboy@gmail.com>
2022-09-08 16:52:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author='scarfboy',
     author_email='scarfboy@gmail.com',
     description='Install test',
-    packages=['wetsuite'],    
+    packages=['src/wstest'],    
     install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1', 'spacy', 'spacy-transformers', 'requests'], # merely suggested: 'easyocr', 
 )
