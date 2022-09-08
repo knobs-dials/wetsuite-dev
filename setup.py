2bb005cc42375f5e0018ffcfeaf5899035509b0f
Bart <scarfboy@gmail.com>
2022-09-08 16:50:49 +0200

    fiddling...

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wetsuite',
+    name='wstest',
     version='0.0.10',
     url='https://github.com/scarfboy/wet-test.git',
     author='scarfboy',
