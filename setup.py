523cc0d4c574e9cd4f91621de57ab65dc7a3805e
scarfboy <scarfboy@gmail.com>
2022-09-15 17:22:32 +0200

    Update setup.py Update setup.py Update setup.py
    ...oh
    Update setup.py
    Delete __init__.py
    removing unnecessary directory
    
    Update setup.py
    Update setup.py
    Create setup.cfg
    Delete setup.cfg
    Update setup.py
    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wetsuite',
+    name='wstest',
     version='0.0.10',
+    classifiers=['Development Status :: 2 - Pre-Alpha', 'Programming Language :: Python :: 3', 'Topic :: Text Processing :: Linguistic'],
     url='https://github.com/scarfboy/wetsuite.git',
     author='scarfboy',
     author_email='scarfboy@gmail.com',
     description='Install test',
-    packages=['datasets'],
+    packages=['wetsuite.datasets', 'wetsuite.helpers'],
     package_dir={"": "src"},
+    python_requires=">=3",
     install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1', 'spacy', 'spacy-transformers', 'requests'], # merely suggested: 'easyocr', 
     extras_require={
         'spacy-cpu': 'spacy',
