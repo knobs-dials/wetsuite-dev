e36d8d7c15e021e4966db743b89748294ac79d95
scarfboy <scarfboy@gmail.com>
2022-09-15 16:52:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setup(
     author='scarfboy',
     author_email='scarfboy@gmail.com',
     description='Install test',
-    packages=['datasets', 'helpers'],
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
+    python_requires=">=3",
     install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1', 'spacy', 'spacy-transformers', 'requests'], # merely suggested: 'easyocr', 
     extras_require={
         'spacy-cpu': 'spacy',
