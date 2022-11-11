b3fe34e76813a92b24a40eb2934053a36252b972
Bart <scarfboy@gmail.com>
2022-11-11 12:34:15 +0100

    adding datacollect to be packaged

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='scarfboy',
     author_email='scarfboy@gmail.com',
     description='Wetsuite',
-    packages=['wetsuite.datasets', 'wetsuite.helpers', 'wetsuite.phrases'],
+    packages=['wetsuite.datasets', 'wetsuite.helpers', 'wetsuite.phrases', 'wetsuite.datacollect'],
     package_dir={"": "src"},
     python_requires=">=3",
     install_requires=['requests', 'numpy >= 1.11.1', 'matplotlib >= 1.5.1', 'spacy', 'spacy-transformers' ],
