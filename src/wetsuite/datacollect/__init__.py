d9113440755b4ac3b84f8d01f9a7e6bee9514494
Bart <scarfboy@gmail.com>
2022-11-11 13:13:51 +0100

    needs an init to be a package

---
+++ src/wetsuite/datacollect/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''
+Code that helps collect data from various sources.
+
+A good portion of this may just be showing our work of how we create dataset,
+but some of it may be usable to create more custom datasets yourself.
+'''
