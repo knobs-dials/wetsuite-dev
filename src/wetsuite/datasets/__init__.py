5755a01e544e22c62d448a4512dd39831756e0bc
Bart <scarfboy@gmail.com>
2022-11-23 12:38:13 +0100

    changed temporary hosting's domain name.

--- src/wetsuite/datasets/__init__.py
+++ src/wetsuite/datasets/__init__.py
@@ -5,6 +5,8 @@
     so there is an describe() to get you started, that each dataset should fill out.
 
     TODO: 
+    * rewrite as class instead of using globals, it's a little cleaner
+    * decide how often to re-fetch the index. Each interpreter (which is what it does right now)? Store and base on mtime?
     * think about having stable datasets, for things like benchmarking
       maybe "any dataset that has a date is fixed, any other may be updated over time" ?
 
@@ -44,31 +46,40 @@ def hash(data):
 
 
 def load(dataset_name, show_progress=True):
-    ''' Loads a dataset into memory, by name.   
-
-        Which may involve fetching it first. Fetched datasets are cached in your home directory.
+    ''' Loads a dataset into memory, by name.
+    
+        First time it's downloaded.
+        It's cached in your home directory.
     '''
-    ### Figure out where it is, or would be, stored
+    global _index
+    if _index == None:
+        _index = fetch_index()
+
+    if dataset_name not in _index:
+        raise ValueError("Do not know dataset name %r"%dataset_name)
+
+    ### Figure out where the dataset is stored, or should be
+    ## check directory
     home_dir = os.path.expanduser("~")
     data_dir = os.path.join( home_dir, '.wetsuite', 'datasets' )
 
-    # TODO: more permission checking so we could give clearer errors?
     if not os.path.exists( data_dir ): 
         os.makedirs( data_dir )
 
     if not os.access(data_dir, os.W_OK):
         raise OSError("We cannot write to our data directory, %r"%data_dir)
+    # TODO: more permission checking, so we could give clearer errors
 
-    if dataset_name not in _index:
-        raise ValueError("Do not know dataset name %r"%dataset_name)
 
+    ## figure out path in that directory
     dataset_details = _index[dataset_name]
     data_url        = dataset_details['url']
-    location_hash   = hash( data_url.encode('utf8') )
+    location_hash   = hash( data_url.encode('utf8') ) 
 
+    # If we don't have it in our cache, then download it.
+    data_path = os.path.join( data_dir, location_hash )      # CONSIDER: using dataset_name instead of location_hash  (BUT that would mean restricting the characters allowed in there)
+    # right now the data_path is a single file per dataset, expected to be a JSON file.
 
-    ### If we don't have it in our cache, then download it.
-    data_path = os.path.join( data_dir, location_hash )
     if not os.path.exists( data_path ):
         
         #with open(data_path,'wb') as f:
@@ -79,15 +90,15 @@ def load(dataset_name, show_progress=True):
 
         wetsuite.helpers.net.download( data_url, tofile_path=tmp_path, show_progress=show_progress)
 
-        ### THINK: it may be preferable to store it compressed, and decompress every load. Or at least make this a parameter
-        # if it was compressed, decompress it in the cache
+        ## if it was compressed, decompress it in the cache - as part of the download, not the load
+        # THINK: it may be preferable to store it compressed, and decompress every load. Or at least make this a parameter
         if data_url.endswith('.bz2'):
             #print('Decompressing...', file=sys.stderr)
             uncompressed_data_bytes = 0
             with bz2.BZ2File(tmp_path, 'rb') as compressed_file_object:
                 with open(data_path,'wb') as write_file_object:
                     while True:
-                        data = compressed_file_object.read( 1048576 )
+                        data = compressed_file_object.read( 2*1048576 )
                         if len(data)==0:
                             break
                         write_file_object.write(data)
@@ -97,10 +108,9 @@ def load(dataset_name, show_progress=True):
 
             print('  done.', file=sys.stderr)
             os.unlink( tmp_path )
-        # CONSIDER: add gz, zip, because they're standard library anyway
+        # CONSIDER: add gz and zip cases, because they're standard library anyway
         else: # assume it was uncompressed
             os.rename( tmp_path, data_path )
-
 
     ### Finally the real bit: read from disk, and return.
     with open(data_path) as f:
@@ -110,18 +120,18 @@ def load(dataset_name, show_progress=True):
 
 
 ### Index of current datasets
-# this needs to become a remotely stored thing.  Right now it's hardcoded because I'm figuring out the loading API to be in general,
+# This needs to become a remotely stored thing.  
+#   right now it's hardcoded because I'm figuring out the loading API in general
 
-_index_url = 'https://'
+_index_url = 'https://https://wetsuite.knobs-dials.com/datasets/index.json'
 
-_index = {
-    'kamervragen':{  'url':'https://beep.knobs-dials.com/datasets/kamervragen.json.bz2',   'description':'',   'version':'preliminary'  }
-}
+_index = None
 
 
 def list_datasets():
+    global _index
     if _index==None:
-        fetch_index()
+        _index = fetch_index()
     return _index.keys()
 
 
@@ -131,11 +141,16 @@ def fetch_index():
 
         Current hosting is on github. TODO: keep it generic so that any hoster will do?
     '''
+    return {
+        'kamervragen':       {  'url':'https://wetsuite.knobs-dials.com/datasets/kamervragen.json.bz2',  'version':'preliminary', 'short_description':'',    }
+        #'gemeente-list-2022':{  'url':'https://wetsuite.knobs-dials.com/datasets/gemeentes-2022.json.bz2',    'version':'preliminary', 'short_description':'List of municipalities',    }
+    }
+
+
     index_data = wetsuite.helpers.net.download( _index_url )
 
     try:
         index_dict = json.loads( index_data )
     except:
         raise
-
 
