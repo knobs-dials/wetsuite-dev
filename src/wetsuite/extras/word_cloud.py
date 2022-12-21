1aa18c01c939ddca42e1657379ff5cd855d69cb8
Bart <scarfboy@gmail.com>
2022-12-21 15:35:03 +0100

    cleaned up word_cloud logic a bit

--- src/wetsuite/extras/word_cloud.py
+++ src/wetsuite/extras/word_cloud.py
@@ -6,53 +6,82 @@ import wordcloud #  if not installed, do  pip3 install wordcloud
 
 
 def wordcloud_from_freqs(freqs: dict, width:int=1800, height:int=800):
-    ''' Takes a string->count dict  (probably from one of the helper functions) 
+    ''' Takes a {string: count} dict  (probably from one of the helper functions) assumed to be filtered nicely already.
         Returns an image.
     '''
-    wco = wordcloud.WordCloud( width = width,  height = height,  background_color = 'white',  min_font_size = 10 )
+    wco = wordcloud.WordCloud( width=width,  height=height,  background_color='white',  min_font_size=10 )
     im = wco.generate_from_frequencies( freqs )
     return im.to_image()
 
 
+stopwords_en = wordcloud.STOPWORDS
+stopwords_nl = ('de','het','een', 'en','of', 'die','van', 'op','aan','door','voor','tot','bij', 'kan','wordt')
 
-def count_case_insensitive(strings: List[str]) -> dict:
+
+
+def count_normalized(strings: List[str], min_count:int=1, min_word_length=2, normalize_func=None, stopwords=None) -> dict:
     ''' Takes a list of strings, returns a { string: count } dict.
-        Specifically, it count strings as if they were case-insensitive, but report the most common case of capitalisation 
+
+        By default, this counts strings as if they were case-insensitive, but report the most common case of capitalisation,
+        which is due to the default normalize_func.
 
         For example, count_case_insensitive( 'a A A a A A a B b b B b'.split()) 
         will give {'A':7, 'b':5}
 
-        TODO: merge logic with freqs_simple
+        
+         normalize_func: should be a str->str function.   If you hand in None (default), it defaults to apply str.lower()
+
+        min_word_length: strings shorter than this are not counted.
+                         This is tested after normalization, so you can remove things in normalization too.
+
+              min_count: if the final count is < min_count, it does not show up in the results 
+                         CONSIDER: top-n, or float as percentile or such
+        
+              stopwords: defaults to some English and Dutch stopwords, handing in a list uses yours instead 
+                         hand in an empty list or tuple to not use stopwords
+                         CONSIDER: have case insensitive and case sensitive stopwords
+                         Note that if you're using spacy or other POS tagging, 
+                         filtering e.g. just nouns and such before handing it into this is a lot cleaner and easier, if a little slower.
+
+            CONSIDER: imitating wordcloud's collocations= behaviour
+            CONSIDER: imitating wordcloud's normalize_plurals=True
+            CONSIDER: imitating wordcloud's include_numbers=False
     '''
-    count = collections.defaultdict(lambda: collections.defaultdict(int)) # { lower form -> { real form -> int } }
+    if normalize_func is None: 
+        normalize_func = lambda s:s.lower()
+    # else assume it's a useful function
+
+    stop = set()
+    if stopwords is None:
+        stop.update(stopwords_en)
+        stop.update(stopwords_nl)
+    else:
+        stop.update(stopwords)
+
+    count = collections.defaultdict(lambda: collections.defaultdict(int)) # { lower_form: { real_form: count } }
     for string in strings:
-        count[string.lower()][string] += 1
+        if string in stop:
+            continue
+        norm_string = normalize_func(string)
+        if len(norm_string) < min_word_length:
+            continue
+        count[ norm_string ][ string ] += 1
     ret = {}
     for ls in count:
         variants = sorted( count[ls].items(), key=lambda x:x[1], reverse=True )
-        ret[ variants[0][0] ] = sum( cnt  for _,cnt in variants)       
+        sum_count = sum( cnt  for _,cnt in variants)
+        if sum_count >= min_count:
+            ret[ variants[0][0] ] = sum_count
     return ret
 
 
 
-def freqs_simple(text:str, stopwords=None) -> dict:
-    ''' Takes text, 
-        Returns a string->count dict   using fairly simple word splitting, and removing stoplist words and numbers
-    '''
-    stop = set()
-    if stopwords is None:
-        stop.update(wordcloud.STOPWORDS) # english stopwords
-        stop.update(['de','het','een', 'en','of', 'die','van', 'op','aan','door','voor','tot','bij', 'kan','wordt'])
-    else:
-        stop.update(stopwords)
+def freqs_from_spacy_document(doc_or_sequence_of_docs, remove_stop=True, restrict_to_tags=('NOUN', 'PROPN', 'ADJ', 'ADV', 'VERB'), weigh_deps={'nsubj':5, 'obj':3} ) -> dict:
+    ''' Returns a string->count dict   
+        Tries to to be smarter about selecting useful words 
 
-    wco = wordcloud.WordCloud( stopwords = stop, normalize_plurals=True, collocations=True, include_numbers=False, min_word_length=2 )
-    return wco.process_text(text)
+        CONSIDER: make this a filter instead, so we can feed the result to count_normalized()
 
-
-def freqs_from_spacy_document(doc_or_sequence_of_docs, remove_stop=True, restrict_to_tags=('NOUN', 'ADJ', 'ADV', 'VERB'), weigh_deps={'nsubj':5, 'obj':3} ) -> dict:
-    ''' Returns a string->count dict   trying to be smarter about selecting useful words 
-    
         CONSIDER: whether half of that can be part of some topic-modeling filtering. And how filters might work around spacy.
     '''
     counts = collections.defaultdict(int)
@@ -68,7 +97,6 @@ def freqs_from_spacy_document(doc_or_sequence_of_docs, remove_stop=True, restric
             if remove_stop and token.is_stop:
                 #print( "SKIP %r - is stopword"%token.text)
                 continue
-            
             if restrict_to_tags is not None  and  token.pos_ not in restrict_to_tags:
                 #print( "SKIP %r - based on tag %s"%(token.text, token.pos_))
                 continue
@@ -96,17 +124,21 @@ def freqs_from_spacy_document(doc_or_sequence_of_docs, remove_stop=True, restric
 
         
 if __name__ == '__main__':
-    import sys
-    # quick and dirty tests from filenames or stdin - no language processing beyond stoplists
-    if len(sys.argv) > 1:
-        for fn in sys.argv[1:]:
-            with open(fn) as f:
-                freqs = freqs_simple( f.read() )
-            im = wordcloud_from_freqs(freqs)
-            im.show()
-    else:
-        text = sys.stdin.read()
-        freqs = freqs_simple( text )
+    # quick and dirty tests from text files
+
+    import sys, re
+
+    def simple_tokenize(text: str):  # quick and dirty tokenizer
+        ' split string into words '
+        l = re.split('[\\s!@#$%^&*":;/,?\xab\xbb\u2018\u2019\u201a\u201b\u201c\u201d\u201e\u201f\u2039\u203a\u2358\u275b\u275c\u275d\u275e\u275f\u2760\u276e\u276f\u2e42\u301d\u301e\u301f\uff02\U0001f676\U0001f677\U0001f678-]+', text)
+        return list(e.strip("'")   for e in l  if len(e)>0)
+
+    for fn in sys.argv[1:]:
+        with open(fn) as f:
+            filedata = f.read()
+            toks = simple_tokenize( filedata )
+            freqs = count_normalized( toks, min_count=2, normalize_func=lambda s:s.lower().strip('([])') )
+            #print( freqs )
         im = wordcloud_from_freqs(freqs)
         im.show()
 
