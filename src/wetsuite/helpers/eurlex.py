0a8cd81b3e745faef7d8117ca37a90d603d09d58
scarfboy <scarfboy@gmail.com>
2023-06-11 15:15:21 +0200

    robust to some missing sections

--- src/wetsuite/helpers/eurlex.py
+++ src/wetsuite/helpers/eurlex.py
@@ -48,6 +48,8 @@ def extract_html(htmlbytes):
     ret['dates']={}
     parsed_dates = {}
     for what, val in parse_datalist( PPDates_Contents.find('dl') ).items():
+        if ';' in val:
+            val = val.split(';')[0].strip()
         if '/' in val: # format ISO8601 style for less ambiguity
             val = datetime.datetime.strptime(val, "%d/%m/%Y").strftime('%Y-%m-%d')
         ret['dates'][what] = val
@@ -55,8 +57,13 @@ def extract_html(htmlbytes):
     PPMisc_Contents = soup.find(id='PPMisc_Contents')
     ret['misc'] = parse_datalist( PPMisc_Contents.find('dl') )
 
+
     PPProc_Contents = soup.find(id='PPProc_Contents')
-    ret['proc'] = parse_datalist( PPProc_Contents.find('dl') )
+    proc = []
+    if PPProc_Contents is not None:
+        proc = parse_datalist( PPProc_Contents.find('dl') )
+    ret['proc'] = proc
+
 
     PPLinked_Contents = soup.find(id='PPLinked_Contents')
     parsed = {}
@@ -71,92 +78,125 @@ def extract_html(htmlbytes):
             parsed[what] = val
     ret['linked'] = parsed
 
+
+    # Doctrine
     PPDoc_Contents = soup.find(id='PPDoc_Contents')
     parsed = {}
-    for what, val in parse_datalist(PPDoc_Contents.find('dl')).items():
-        if type(val) is bs4.element.ResultSet:
-            parsedval = []
-            for li in val:
-                a = li.find('a')
-                parsedval.append(  li.text  ) # TODO: check that doesn't need to be a join-findall too
-            parsed[what] = parsedval
-        else:
-            parsed[what] = val
+    if PPDoc_Contents is not None:
+        for what, val in parse_datalist(PPDoc_Contents.find('dl')).items():
+            if type(val) is bs4.element.ResultSet:
+                parsedval = []
+                for li in val:
+                    a = li.find('a')
+                    parsedval.append(  li.text  ) # TODO: check that doesn't need to be a join-findall too
+                parsed[what] = parsedval
+            else:
+                parsed[what] = val
     ret['doctrine'] = parsed
 
+
+    # Classifications
     PPClass_Contents = soup.find(id='PPClass_Contents')
     parsed = {}
-    for what, val in parse_datalist(PPClass_Contents.find('dl')).items():
-        if type(val) is bs4.element.ResultSet:
-            parsedval = []
-            for li in val:
-                div = li.find('div')
-                if div is not None:
-                    parsedval.append(  list(s.strip()   for s in div.findAll(text=True)  if len(s.strip())>0 )  )
-                else:
-                    parsedval.append( ''.join( li.findAll(text=True) ).strip() )  
-            parsed[what] = parsedval
-        else:
-            parsed[what] = val
+    if PPClass_Contents is not None:
+        for what, val in parse_datalist(PPClass_Contents.find('dl')).items():
+            if type(val) is bs4.element.ResultSet:
+                parsedval = []
+                for li in val:
+                    div = li.find('div')
+                    if div is not None:
+                        parsedval.append(  list(s.strip()   for s in div.findAll(text=True)  if len(s.strip())>0 )  )
+                    else:
+                        parsedval.append( ''.join( li.findAll(text=True) ).strip() )  
+                parsed[what] = parsedval
+            else:
+                parsed[what] = val
     ret['classifications'] = parsed
 
 
-    #Languages and formats available
+    # Languages and formats available   (not always there)
     PP2Contents = soup.find(id='PP2Contents')
     contents = []
-    for ul in PP2Contents.findAll('ul'):
-        format = None
-        for maybe_format in ul.get('class'):
-            if maybe_format.startswith('PubFormat'):
-                format = maybe_format[9:]
-        if format != None:
-            for li in ul.findAll('li'):
-                if 'disabled' not in li.get('class',''):
-                    a = li.find('a')
-                    lang = a.find('span').text
-                    if format == 'VIEW':
-                        continue
-                    # constructing the URL like that is cheating and may not always work. Ideally we'd urllib.parse.urljoin  it from the href, but then we must know the URL this was fetched from.
-                    contents.append( (lang, format, 'https://eur-lex.europa.eu/legal-content/%s/TXT/%s/?uri=CELEX:%s'%(lang, format, celex)) )
+    if PP2Contents is not None:
+        for ul in PP2Contents.findAll('ul'):
+            format = None
+            for maybe_format in ul.get('class'):
+                if maybe_format.startswith('PubFormat'):
+                    format = maybe_format[9:]
+            if format != None:
+                for li in ul.findAll('li'):
+                    if 'disabled' not in li.get('class',''):
+                        a = li.find('a')
+                        lang = a.find('span').text
+                        if format == 'VIEW':
+                            continue
+                        # constructing the URL like that is cheating and may not always work. Ideally we'd urllib.parse.urljoin  it from the href, but then we must know the URL this was fetched from.
+                        contents.append( (lang, format, 'https://eur-lex.europa.eu/legal-content/%s/TXT/%s/?uri=CELEX:%s'%(lang, format, celex)) )
     ret['contents'] = contents
 
+
+    #Document text  (not always there)
     PP4Contents = soup.find(id='PP4Contents')
-    TexteOnly = PP4Contents.find(id='TexteOnly')
-
     txt = []
-    section_name, section_txt = '', []
+    if PP4Contents is not None:
+        doc = PP4Contents.find('p').parent    #TexteOnly = PP4Contents.find(id='TexteOnly')
+        section_name, section_txt = '', []
+        for node in doc.children:
+            if type(node) is bs4.element.NavigableString:
+                s = node.string.strip()
+                if len(s)>0:
+                    txt.append(s)
+            else: # assume Tag
+                if node.name in ('h2',):
+                    if len(section_txt) > 0:
+                        txt.append( (section_name, section_txt) )
+                    section_name, section_txt = '', []
+                    section_name = node.text
 
-    for node in TexteOnly.children:
+                elif node.name in ('p',):
+                    txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
+                    section_txt.extend( txtfrags )
+                elif node.name in ('em',):
+                    txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
+                    section_txt.extend( txtfrags )
+                elif node.name in ('br','hr'):
+                    pass # is nothing
+                elif node.name in ('a',): # seem to be used mainly as anchors for browsers to #go to, so skippable
+                    if len(node.text.strip())>0:
+                        raise ValueError("Bad assumption, that <a> has no text, in %r"%node) 
 
-        if type(node) is bs4.element.NavigableString:
-            s = node.string.strip()
-            if len(s)>0:
-                txt.append(s)
-        else: # assume Tag
-            if node.name in ('h2',):
-                if len(section_txt) > 0:
-                    txt.append( (section_name, section_txt) )
-                section_name, section_txt = '', []
+                # not really inspected, add flattened for now
+                elif node.name in (
+                    'title',
+                    'div',
+                    'span',
+                    'table',
+                    'dl','dt','dd',
+                ):
+                    #print( node.name.upper(), node)
+                    txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
+                    section_txt.extend( txtfrags )
 
-                section_name = node.text
-            elif node.name in ('p',):
-                txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
-                section_txt.extend( txtfrags )
-            elif node.name in ('em',):
-                txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
-                section_txt.extend( txtfrags )
-            elif node.name in ('br',):
-                pass # is nothing
-            elif node.name in ('a',): # seem to be used mainly as anchors for browsers to #go to, so skippable
-                if len(node.text.strip())>0:
-                    raise ValueError("Bad assumption, that <a> has no text, in %r"%node) 
-            else:
-                raise ValueError( "Don't yet handle %r"%node.name  )
+                # ignore
+                elif node.name in ('link',): # seems to be stylesheets
+                    #print('LINK', node)
+                    pass
+                elif node.name in ('meta', # probably just a charset?
+                                   'font'): 
+                    #print('META', node)
+                    pass
 
-    if len(section_txt) > 0:
-        txt.append( (section_name, section_txt) )
-    section_name, section_txt = '', []
-    
+                elif node.name is None:
+                    print('NONE', node)
+
+                else:
+                    raise ValueError( "Don't yet handle %r"%node.name  )
+
+        if len(section_txt) > 0:
+            txt.append( (section_name, section_txt) )
+        section_name, section_txt = '', []
+        
     ret['text'] = txt
+
 
     return ret
