9f57a91fd3b3f8808f49124fb48ece51cce685e5
scarfboy <scarfboy@gmail.com>
2023-06-25 11:16:32 +0200

    figuring out licenses for compatibility reasons

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='wetsuite',
     version='0.0.10',
-    classifiers=['Development Status :: 2 - Pre-Alpha', 'Programming Language :: Python :: 3', 'Topic :: Text Processing :: Linguistic'],
+    classifiers=['Development Status :: 2 - Pre-Alpha',   'Programming Language :: Python :: 3',   'Topic :: Text Processing :: Linguistic'],
     url='https://github.com/scarfboy/wetsuite.git',
     author='scarfboy',
     author_email='scarfboy@gmail.com',
@@ -11,16 +11,27 @@ setup(
     packages=['wetsuite.datasets', 'wetsuite.helpers', 'wetsuite.phrases', 'wetsuite.datacollect', 'wetsuite.extras'],
     package_dir={"": "src"},
     python_requires=">=3",
-    install_requires=['lxml', 'requests', 'numpy >= 1.11.1', 'matplotlib >= 1.5.1', 'spacy', 'spacy-transformers', 'python-dateutil' ],
+    install_requires=[
+        'lxml',                # BSD
+        'requests',            # Apache2
+        'python-dateutil',     # Apache2
+        'numpy >= 1.11.1',     # BSD   
+        'matplotlib >= 1.5.1', # BSD       
+        'spacy',               # MIT
+        'spacy-transformers',  # MIT       
+    ],
     extras_require={
         'spacy-cpu':'spacy',
         'spacy-cuda80':'spacy[cuda80]',
         'spacy-cuda90':'spacy[cuda90]','spacy-cuda91':'spacy[cuda91]','spacy-cuda92':'spacy[cuda92]',
         'spacy-cuda100':'spacy[cuda100]','spacy-cuda101':'spacy[cuda101]','spacy-cuda102':'spacy[cuda102]','spacy-cuda110':'spacy[cuda110]',
         'spacy-cuda111':'spacy[cuda111]','spacy-cuda112':'spacy[cuda112]','spacy-cuda113':'spacy[cuda113]','spacy-cuda114':'spacy[cuda114]','spacy-cuda115':'spacy[cuda115]','spacy-cuda116':'spacy[cuda116]','spacy-cuda117':'spacy[cuda117]',
-        'fastlang': ['spacy_fastlang','fasttext'],
-        'ocr':'easyocr',
-        'extras':['wordcloud'],
+        'fastlang':[
+            'spacy_fastlang',   # MIT
+            'fasttext'          # MIT
+        ],
+        'ocr':'easyocr',        # Apache2
+        'extras':['wordcloud',], # MIT
         # all?
     },
 )
