a9500c80779dbe9f52273ccd0143745b20cbc00e
scarfboy <scarfboy@gmail.com>
2023-08-09 16:56:55 +0200

    guards for potentially missing parts - which is basically all of them

--- src/wetsuite/helpers/eurlex.py
+++ src/wetsuite/helpers/eurlex.py
@@ -5,28 +5,54 @@ import requests
 import bs4
 
 
-def fetch_JUDG():
-    ''' Returns some specifically structured
+
+
+def fetch_by_resource_type(typ='JUDG'):
+    ''' Intends to queries the SPARQL endpoint to ask for most CELEXes of a specific type, here court judgments, 
+        
+        typ can e.g. be 
+        - 'JUDG'  for judgments
+        - 'REG'   for regulations (but there are a handful of related things) 
+        and anything else 
+
+        TODO: fetch values e.g. at 
+        https://github.com/SEMICeu/Excel-to-CPSVAP-RDF-transformation/blob/master/page-objects/utils/CPSVtemplateWithCodelists.json
+        in handier form
+
+
+        Asks to give its semantic results as JSON data,  which we parse and return as a python structure.
+        The structure you get back looks like:  ( see also see also https://www.w3.org/TR/2013/REC-sparql11-results-json-20130321/ )
+        {
+            'head': {
+                'link': [], 'vars': ['work', 'type', 'celex', 'date', 'force']
+            },
+            'results': {
+                'distinct': False,
+                'ordered': True,
+                'bindings': [
+                    {'work': {'type': 'uri',           'value':'http://publications.europa.eu/resource/cellar/1e3100ce-8a71-433a-8135-15f5cc0e927c'},
+                    'type': {'type': 'uri',           'value':'http://publications.europa.eu/resource/authority/resource-type/JUDG'},
+                    'celex': {'type': 'typed-literal', 'value':'61996CJ0080', 'datatype': 'http://www.w3.org/2001/XMLSchema#string'},
+                    'date': {'type': 'typed-literal', 'value':'1998-01-15',  'datatype': 'http://www.w3.org/2001/XMLSchema#date'}
+                    },
+                    # ...one of these for each result
+                ]
+            }
+        }
     '''
     # The proper way would be to use a library like sparqlwrapper
-    # but for now we can get away with things like:
+    #   but for now we can get away with hardcodig a query like:
     query = '''PREFIX cdm: <http://publications.europa.eu/ontology/cdm#>
-      PREFIX annot: <http://publications.europa.eu/ontology/annotation#>
-      PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
-      PREFIX dc:<http://purl.org/dc/elements/1.1/>
-      PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
-      PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX owl:<http://www.w3.org/2002/07/owl#>
       select distinct ?work ?type ?celex ?date ?force 
       WHERE {
           ?work cdm:work_has_resource-type ?type. 
-          FILTER(?type=<http://publications.europa.eu/resource/authority/resource-type/JUDG>)
+          FILTER(?type=<http://publications.europa.eu/resource/authority/resource-type/%s>)
           FILTER not exists{?work cdm:work_has_resource-type <http://publications.europa.eu/resource/authority/resource-type/CORRIGENDUM>
       } 
       OPTIONAL { ?work cdm:resource_legal_id_celex ?celex. } 
       OPTIONAL { ?work cdm:work_date_document ?date. } 
       OPTIONAL { ?work cdm:resource_legal_in-force ?force. } 
-      FILTER not exists{?work cdm:do_not_index "true"^^<http://www.w3.org/2001/XMLSchema#boolean>}. }'''
+      FILTER not exists{?work cdm:do_not_index "true"^^<http://www.w3.org/2001/XMLSchema#boolean>}. }'''%typ
 
     url = 'http://publications.europa.eu/webapi/rdf/sparql?default-graph-uri=&query='+urllib.parse.quote(query)+'&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+'
     resp = requests.get(url)
@@ -34,7 +60,7 @@ def fetch_JUDG():
 
 
 def extract_html(htmlbytes):
-    ''' Extract data from formatted HTML.
+    ''' Extract data from formatted HTML from the website itself.
 
         Written for JUDG pages, may need work for others.
 
@@ -44,15 +70,20 @@ def extract_html(htmlbytes):
         TODO: see how language-sensitive this is.
         CONSIDER: extract more link hrefs (would probably need to hand in page url to)
     '''
+    # This code turned messier than it originally was, because the page turned out to be more flexible than I thought.
+
     def parse_datalist(under_dl_node):
         ' pick out the basic parts of a data list '
         ret = {}
         for node in under_dl_node.children:
+            # generally we have a dt used as a label, and dd with a value
             if node.name == 'dt':
                 what = node.text.strip().strip(':')
             elif node.name == 'dd':
+                # however, the dd can contain a list instead of just a string
+                #    the structure is consistent only within each section, so leave it for code calling this function to process usefully  (forced to be - JSON would choke on it if you insert it as-is)
                 if node.find(['ul','ol']):
-                    ret[what] = node.findAll('li') # the structure is consistent only within each section, so leave it for outside code to process
+                    ret[what] = node.findAll('li') 
                 else:
                     ret[what] = node.text.strip()
         return ret
@@ -66,53 +97,84 @@ def extract_html(htmlbytes):
 
     PP1Contents = soup.find(id='PP1Contents')
     ret['titles']={}
-    ret['titles']['title'] = PP1Contents.find(id='title').text
-    ret['titles']['englishTitle'] = PP1Contents.find(id='englishTitle').text
-    ret['titles']['originalTitle'] = PP1Contents.find(id='originalTitle').text
+    if PP1Contents is not None:
+        ret['titles']['title'] = PP1Contents.find(id='title').text
+        ret['titles']['englishTitle'] = PP1Contents.find(id='englishTitle').text
+        ret['titles']['originalTitle'] = PP1Contents.find(id='originalTitle').text
 
-    eid = PP1Contents.find('p', text=re.compile(r'.*ECLI identifier.*'))
-    if eid is not None:
-        ret['ecli'] = eid.text.split(':', 1)[1].strip()
+        eid = PP1Contents.find('p', text=re.compile(r'.*ECLI identifier.*'))
+        if eid is not None:
+            ret['ecli'] = eid.text.split(':', 1)[1].strip()
 
     PPDates_Contents = soup.find(id='PPDates_Contents')
     ret['dates'] = {}
-    for what, val in parse_datalist( PPDates_Contents.find('dl') ).items():
-        if ';' in val:
-            val = val.split(';')[0].strip()
-        if '/' in val: # format ISO8601 style for less ambiguity
-            val = datetime.datetime.strptime(val, "%d/%m/%Y").strftime('%Y-%m-%d')
-        ret['dates'][what] = val
+    if PPDates_Contents is not None:
+        for what, val in parse_datalist( PPDates_Contents.find('dl') ).items():
+            if ';' in val:
+                val = val.split(';')[0].strip()
+            if '/' in val: # format ISO8601 style for less ambiguity
+                val = datetime.datetime.strptime(val, "%d/%m/%Y").strftime('%Y-%m-%d')
+            ret['dates'][what] = val
 
     PPMisc_Contents = soup.find(id='PPMisc_Contents')
-    ret['misc'] = parse_datalist( PPMisc_Contents.find('dl') )
+    ret['misc'] = {}
+    if PPMisc_Contents is not None:
+        ret['misc'] = parse_datalist( PPMisc_Contents.find('dl') )
 
 
     PPProc_Contents = soup.find(id='PPProc_Contents')
-    proc = []
+    ret['proc'] = {}
     if PPProc_Contents is not None:
-        proc = parse_datalist( PPProc_Contents.find('dl') )
-    ret['proc'] = proc
+        # procedure looks like a key:value thing (e.g. Defendant:Raad), 
+        # but there are cases where the value is a list, which parse_datalist doesn't handle for us so we have to.
+        # for consistency's sake, even the single-value cases are returned as a list
+        for k, v in parse_datalist( PPProc_Contents.find('dl') ).items():
+            if type(v) is str:
+                ret['proc'][k] = [v]
+            else: # will be a list of bs4 nodes, e.g.  [<li><a href="./../../../procedure/EN/2018_395">2018/0395/NLE</a></li>] 
+                ret['proc'][k] = []
+                for li in v:
+                    a = li.find('a')
+                    if a is not None:
+                        ret['proc'][k].append( a.text ) # TODO: consider actually figuring out the link
+                print( ret['proc'][k] )
+                #print( k, type(v), v )
+
+            # print( dlitem )
+            # if type(dlitem) in (str, dict): # dict seems typical; TODO: rewrite the other cases to dict
+            #     ret['proc'] = dlitem
+            # else:
+            #     # is still the bs4 object
+            #     # e.g.  in 62020CJ0180
+            #     # ignore for now? TODO: Warn?
+            #     print("WARNING - didn't think proc would have a %s in %s"%(type(dlitem).__class__.__name__, celex))
+            #     print('DLITEM',dlitem)
+            #     raise
+            #     #ret['proc'] = it
 
 
     PPLinked_Contents = soup.find(id='PPLinked_Contents')
-    parsed_link = {}
-    for what, val in parse_datalist(PPLinked_Contents.find('dl')).items():
-        if type(val) is bs4.element.ResultSet:
-            parsedval = []
-            for li in val:
-                a = li.find('a')
-                data_celex = a.get('data-celex')
-                parsedval.append(   (  'CELEX:'+data_celex, ''.join( li.findAll(text=True) ).strip()  )   )
-            parsed_link[what] = parsedval
-        else:
-            parsed_link[what] = val
-    ret['linked'] = parsed_link
+    ret['linked'] = {}
+    if PPLinked_Contents is not None:
+        parsed_link = {}
+        for what, val in parse_datalist(PPLinked_Contents.find('dl')).items():
+            if type(val) is bs4.element.ResultSet:
+                parsedval = []
+                for li in val:
+                    a = li.find('a')
+                    data_celex = a.get('data-celex')
+                    parsedval.append(   (  'CELEX:'+data_celex, ''.join( li.findAll(text=True) ).strip()  )   )
+                parsed_link[what] = parsedval
+            else:
+                parsed_link[what] = val
+        ret['linked'] = parsed_link
 
 
     # Doctrine
     PPDoc_Contents = soup.find(id='PPDoc_Contents')
-    parsed_doctr = {}
+    ret['doctrine'] = {}
     if PPDoc_Contents is not None:
+        parsed_doctr = {}
         for what, val in parse_datalist(PPDoc_Contents.find('dl')).items():
             if type(val) is bs4.element.ResultSet:
                 parsedval = []
@@ -122,13 +184,14 @@ def extract_html(htmlbytes):
                 parsed_doctr[what] = parsedval
             else:
                 parsed_doctr[what] = val
-    ret['doctrine'] = parsed_doctr
+        ret['doctrine'] = parsed_doctr
 
 
     # Classifications
     PPClass_Contents = soup.find(id='PPClass_Contents')
-    parsed_class = {}
+    ret['classifications'] = {}
     if PPClass_Contents is not None:
+        parsed_class = {}
         for what, val in parse_datalist(PPClass_Contents.find('dl')).items():
             if type(val) is bs4.element.ResultSet:
                 parsedval = []
@@ -141,13 +204,14 @@ def extract_html(htmlbytes):
                 parsed_class[what] = parsedval
             else:
                 parsed_class[what] = val
-    ret['classifications'] = parsed_class
+        ret['classifications'] = parsed_class
 
 
     # Languages and formats available   (not always there)
     PP2Contents = soup.find(id='PP2Contents')
-    parsed_contents = []
+    ret['contents'] = []
     if PP2Contents is not None:
+        parsed_contents = []
         for ul in PP2Contents.findAll('ul'):
             format = None
             for maybe_format in ul.get('class'):
@@ -162,69 +226,90 @@ def extract_html(htmlbytes):
                             continue
                         # constructing the URL like that is cheating and may not always work. Ideally we'd urllib.parse.urljoin  it from the href, but then we must know the URL this was fetched from.
                         parsed_contents.append( (lang, format, 'https://eur-lex.europa.eu/legal-content/%s/TXT/%s/?uri=CELEX:%s'%(lang, format, celex)) )
-    ret['contents'] = parsed_contents
+        ret['contents'] = parsed_contents
 
 
     #Document text  (not always there)
     PP4Contents = soup.find(id='PP4Contents')
-    txt = []
+    txt = [] 
     if PP4Contents is not None:
-        doc = PP4Contents.find('p').parent    #TexteOnly = PP4Contents.find(id='TexteOnly')
-        section_name, section_txt = '', []
-        for node in doc.children:
-            if type(node) is bs4.element.NavigableString:
-                s = node.string.strip()
-                if len(s)>0:
-                    txt.append(s)
-            else: # assume Tag
-                if node.name in ('h2',):
-                    if len(section_txt) > 0:
-                        txt.append( (section_name, section_txt) )
-                    section_name, section_txt = '', []
-                    section_name = node.text
 
-                elif node.name in ('p',):
-                    txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
-                    section_txt.extend( txtfrags )
-                elif node.name in ('em',):
-                    txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
-                    section_txt.extend( txtfrags )
-                elif node.name in ('br','hr'):
-                    pass # is nothing
-                elif node.name in ('a',): # seem to be used mainly as anchors for browsers to #go to, so skippable
-                    if len(node.text.strip())>0:
-                        raise ValueError("Bad assumption, that <a> has no text, in %r"%node) 
+        # TODO: review, this may be overkill and/or not complete
+        titerate = []
+        TexteOnly = PP4Contents.find(id='TexteOnly') # probably better if it's there?
+        if TexteOnly is not None:
+            titerate.append( TexteOnly )
+        else: #  currently looks for  div > p    (because p also appears e.g. inside tables)
+            for p in PP4Contents.findAll('p'):
+                #print(p.parent.name)
+                if p.parent.name in ('div',)   and  p.parent not in titerate: # yeah okay, that's nasty
+                    titerate.append( p.parent )
+        
+        # txt will become a list of (section_name_str, section_contents_strlist)
+        #   and all the parts will collect into:
+        cur_section_name, cur_section_txt = '', []
 
-                # not really inspected, add flattened for now
-                elif node.name in (
-                    'title',
-                    'div',
-                    'span',
-                    'table',
-                    'dl','dt','dd',
-                ):
-                    #print( node.name.upper(), node)
-                    txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
-                    section_txt.extend( txtfrags )
+        for iterate_under in titerate:
+            for node in iterate_under.children:
+                if type(node) is bs4.element.NavigableString:
+                    s = node.string.strip()
+                    if len(s)>0:
+                        cur_section_txt.append(s)
+                else: # assume Tag
+                    if node.name in ('h2',
+                                     'h3' # arguably this should not split?
+                                     ):
+                        if len(cur_section_txt) > 0: # flush
+                            txt.append( (cur_section_name, cur_section_txt) )
+                        cur_section_name, cur_section_txt = '', []
+                        cur_section_name = node.text
 
-                # ignore
-                elif node.name in ('link',): # seems to be stylesheets
-                    #print('LINK', node)
-                    pass
-                elif node.name in ('meta', # probably just a charset?
-                                   'font'): 
-                    #print('META', node)
-                    pass
+                    elif node.name in ('p',):
+                        txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
+                        cur_section_txt.extend( txtfrags )
+                    elif node.name in ('em','b','i',
+                                       'center',
+                                       ):
+                        txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
+                        cur_section_txt.extend( txtfrags )
+                    elif node.name in ('br','hr'):
+                        pass # is nothing
+                    elif node.name in ('a',): # seem to be used mainly as anchors for browsers to #go to, so skippable
+                        if len(node.text.strip())>0:
+                            cur_section_txt.extend( node.text.strip() ) # probably used as a header
+                            #raise ValueError("Bad assumption, that an  a  tag has no text, in %r"%(node)) 
 
-                elif node.name is None:
-                    print('NONE', node)
+                    # not really inspected, add flattened for now
+                    elif node.name in (
+                        'title',
+                        'div',
+                        'span',
+                        'table',
+                        'dl','dt','dd',
+                    ):
+                        #print( node.name.upper(), node)
+                        txtfrags = list(  frag  for frag in node.findAll(text=True) if len(frag.strip())>0)
+                        cur_section_txt.extend( txtfrags )
 
-                else:
-                    raise ValueError( "Don't yet handle %r"%node.name  )
+                    # ignore
+                    elif node.name in ('img',):
+                        pass
+                    elif node.name in ('link',): # seems to be stylesheets
+                        #print('LINK', node)
+                        pass
+                    elif node.name in ('meta', # probably just a charset?
+                                    'font'): 
+                        #print('META', node)
+                        pass
 
-        if len(section_txt) > 0:
-            txt.append( (section_name, section_txt) )
-        section_name, section_txt = '', []
+                    elif node.name is None:
+                        print('NONE', node)
+
+                    else:
+                        raise ValueError( "Don't yet handle %r"%node.name  )
+
+        if len(cur_section_txt) > 0: # final flush
+            txt.append( (cur_section_name, cur_section_txt) )
         
     ret['text'] = txt
 
