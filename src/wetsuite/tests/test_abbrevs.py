2b691d6dba3ea6adb342d6caa9535331aaaa7e7b
scarfboy <scarfboy@gmail.com>
2023-11-05 16:43:04 +0100

    module name change

--- src/wetsuite/tests/test_abbrevs.py
+++ src/wetsuite/tests/test_abbrevs.py
@@ -1,21 +1,21 @@
-import wetsuite.phrases.abbreviations
+import wetsuite.helpers.abbreviations
 
 def test_form1():
-    assert wetsuite.phrases.abbreviations.find_abbrevs( "A Bree Veation (ABV)" ) == [('ABV', ['A', 'Bree', 'Veation'])]  
+    assert wetsuite.helpers.abbreviations.find_abbrevs( "A Bree Veation (ABV)" ) == [('ABV', ['A', 'Bree', 'Veation'])]  
 
 def test_form2():
-    assert wetsuite.phrases.abbreviations.find_abbrevs( "(ABV) A Bree Veation" ) == [('ABV', ['A', 'Bree', 'Veation'])] 
+    assert wetsuite.helpers.abbreviations.find_abbrevs( "(ABV) A Bree Veation" ) == [('ABV', ['A', 'Bree', 'Veation'])] 
 
 def test_form3():
-    assert wetsuite.phrases.abbreviations.find_abbrevs( "ABV (A Bree Veation)" ) == [('ABV', ['A', 'Bree', 'Veation'])]  
+    assert wetsuite.helpers.abbreviations.find_abbrevs( "ABV (A Bree Veation)" ) == [('ABV', ['A', 'Bree', 'Veation'])]  
 
 def test_form4():
-    assert wetsuite.phrases.abbreviations.find_abbrevs( "(A Bree Veation) ABV" ) == [('ABV', ['A', 'Bree', 'Veation'])] 
+    assert wetsuite.helpers.abbreviations.find_abbrevs( "(A Bree Veation) ABV" ) == [('ABV', ['A', 'Bree', 'Veation'])] 
 
 def test_count():
     parts = [
-        wetsuite.phrases.abbreviations.find_abbrevs( "A Bree Veation (ABV) and (ABV) A Bree Veation" ),
-        wetsuite.phrases.abbreviations.find_abbrevs( "ABV (A Bree Veation) and (A Bree Veation) ABV" ) 
+        wetsuite.helpers.abbreviations.find_abbrevs( "A Bree Veation (ABV) and (ABV) A Bree Veation" ),
+        wetsuite.helpers.abbreviations.find_abbrevs( "ABV (A Bree Veation) and (A Bree Veation) ABV" ) 
     ]
-    counts = wetsuite.phrases.abbreviations.count_results( parts )  # counts how many documents contain it
+    counts = wetsuite.helpers.abbreviations.count_results( parts )  # counts how many documents contain it
     assert counts == {'ABV': {('A', 'Bree', 'Veation'): 2}}
