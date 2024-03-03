2c1c8c75e7d6517b17aeeb1ac16fc231d192cf53
scarfboy <scarfboy@gmail.com>
2024-03-03 16:30:16 +0100

    wording

--- src/wetsuite/datacollect/__init__.py
+++ src/wetsuite/datacollect/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-''' Assists collection of data from various sources. Actual use of this (and of collection that I{doesn't} need these modules) is demonstrated in the notebooks section.
+'''
+    A collection of helper code that assists collection of data from varied sources.
 
-    A good portion of this may just be showing our (messy) work of how we create datasets,
-    yet some of it may be usable to create more custom datasets yourself.
+    Actual use of this (and of collection that I{doesn't} need these modules) 
+    is a related wetsuite-datacollect repository.
 '''
