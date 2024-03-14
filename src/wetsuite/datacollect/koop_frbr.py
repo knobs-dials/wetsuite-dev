4e7c3e01c9c4dfdd69a268085d9e30cee7b3d1c7
scarfboy <scarfboy@gmail.com>
2024-03-14 17:17:47 +0100

    docstrings, mostly

--- src/wetsuite/datacollect/koop_frbr.py
+++ src/wetsuite/datacollect/koop_frbr.py
@@ -15,22 +15,33 @@ import wetsuite.helpers.localdata
 class FRBRFetcher:
     ''' Helper class to fetch data from an area of https://repository.overheid.nl/frbr/
         See the constructor's docstring for more.
+
+        In theory we could use the bulk service to do functionally the same,
+        which is more efficient for both sides,
+        yet almost no SSH tool seems to be able to negotiate with the way they configured it
+        (SFTP imitating anonymous FTP, which is a grea idea in theory).
     '''
 
     def __init__(self, fetch_store, cache_store, verbose=True, waittime_sec=1.0):
         ''' Hand in two LocalKV style stores: one that the documents will get fetched into,
             and one that the intermediate folders get fetched into
-            (the former is almost all useful content, the latter mostly pointless outside of this fetcher)
+            (the former is almost all useful content, 
+            the latter mostly pointless outside of this fetcher)
 
             After this you will want to 
             - hand in a starting point like::
                 fetcher.add_page( 'https://repository.overheid.nl/frbr/cga?start=1' )
             - use fetcher.work() to start it fetching. 
-              - work() is a generator function that tries to frequently return, so that you can read out some "what have we done"
-                - ( see .count_fetches, .count_cacheds,  .count_items, .count_folders, .count_skipped, .count_pages )
-              - if you just want it to do things until it's done, you can do `list( fetcher.work() )`
+              - work() is a generator function that tries to frequently return, 
+                so that you can read out some "what have we done" ( see .count_* )
+              - if you just want it to do things until it's done, 
+                you can do `list( fetcher.work() )`
 
             Will only go deeper from the starting page you give it.
+            @param fetch_store:
+            @param cache_store:
+            @param verbose:
+            @param waittime_sec: How long to sleep after every actual network fetch, to be nicer to the servers.            
         '''
         self.fetch_store = fetch_store
         self.cache_store = cache_store
@@ -59,10 +70,10 @@ class FRBRFetcher:
             try:
                 self.count_fetches += 1
                 return wetsuite.helpers.net.download( url )
-                #time.sleep(self.waittime_sec)
+                time.sleep(self.waittime_sec)
             except requests.exceptions.Timeout:
                 if self.verbose >= 2:
-                    print("U TRYAGAIN %s"%retries) # trying again just once 'fixes' most cases
+                    print(f"U TRYAGAIN {str(retries)}") # trying again just once 'fixes' most cases
                 retries -= 1
                 time.sleep(1)
 
@@ -72,7 +83,9 @@ class FRBRFetcher:
         retries = max(1, retries)
         while retries > 0:
             try:
-                bytedata, came_from_cache = wetsuite.helpers.localdata.cached_fetch( self.cache_store, url )
+                bytedata, came_from_cache = wetsuite.helpers.localdata.cached_fetch(
+                    self.cache_store, url
+                )
                 if came_from_cache:
                     self.count_cacheds += 1
                     #if self.verbose:
@@ -81,11 +94,11 @@ class FRBRFetcher:
                     self.count_fetches += 1
                     #if self.verbose:
                     #print("CFETCH_FETCHED", url)
-                    #time.sleep(self.waittime_sec)
+                    time.sleep(self.waittime_sec)
                 return bytedata
             except requests.exceptions.Timeout:
                 if self.verbose >= 2:
-                    print("C TRYAGAIN %s"%retries) # trying again just once 'fixes' most cases
+                    print(f"C TRYAGAIN {retries}") # trying again just once 'fixes' most cases
                 retries -= 1
                 time.sleep(1)
             #except urllib3.exceptions.ReadTimeoutError
@@ -93,7 +106,8 @@ class FRBRFetcher:
 
 
     def add_page(self, page_url):
-        ''' add an URL to an internal "pages to still look at" set  (unless it was previously added / fetched)
+        ''' add an URL to an internal "pages to still look at" set  
+            (unless it was previously added / fetched)
             Mostly intended to be used by handle_url()
         '''
         if page_url not in self.fetched:
@@ -103,7 +117,8 @@ class FRBRFetcher:
 
 
     def add_folder(self, folder_url):
-        ''' add an URL to an internal "folders to still look at" set  (unless it was previously added / fetched) 
+        ''' add an URL to an internal "folders to still look at" set  
+            (unless it was previously added / fetched) 
             Mostly intended to be used by handle_url()
         '''
         if folder_url not in self.fetched:
@@ -113,13 +128,17 @@ class FRBRFetcher:
 
 
     def handle_url(self, h_url, is_folder=False):
-        ''' handle what is either a page URL or folder URL 
-        
-        '''
+        ''' handle a URL that should be what we consider either a page or folder '''
         if is_folder:
             pagebytes = self.cached_folder_fetch( h_url )
+            if pagebytes is None:
+                print("ERR cached_folder_fetch( %r ) returned None"%h_url)
         else: # is page
             pagebytes = self.uncached_fetch( h_url )
+            if pagebytes is None:
+                print("ERR uncached_fetch( %r ) returned None"%h_url)
+
+
 
         self.fetched[ h_url ] = True
         soup = bs4.BeautifulSoup( pagebytes, features='lxml' )
@@ -144,11 +163,13 @@ class FRBRFetcher:
                         print( f' ITEM FETCHED {txt:25s}  {fil_absurl}' )
             except ValueError as ve: # probably a 404
                 print( f' ERROR {repr(ve):25s}  {fil_absurl}' )
-            except Exception as e:
+            except Exception as e: # e.g. OperationalError
                 print( f' ERROR TODO {repr(e):25s}  {fil_absurl}' )
 
         # browse items that are folders - recurse
-        folder_soup  = soup.select("div > ul[class*='browse__list'] > li[class*='browse__item'] > a ")
+        folder_soup  = soup.select(
+            "div > ul[class*='browse__list'] > li[class*='browse__item'] > a "
+        )
         folder_names = list(a.find( text=True )  for a in folder_soup)
         chosen_types = wetsuite.helpers.koop_parse.prefer_types( folder_names )
 
@@ -157,9 +178,11 @@ class FRBRFetcher:
             text = a.find( text=True )
             #self.itemlinks[ fol_absurl ] = text
             # TODO: change to 'decide what subset to fetch based on what there is'
-            if text not in chosen_types: # in ('pdf','odt', 'jpg', 'coordinaten', 'ocr'):   # metadata metadataowms  xml html
+            if text not in chosen_types:
+                # in ('pdf','odt', 'jpg','coordinaten','ocr'):
+                # 'metadata' 'metadataowms' 'xml' 'html'
                 self.count_skipped += 1
-                #print( f' SKIP FOLDER: {url}  {text:8s}   {fol_absurl}        (of {folder_names})' )
+                #print( f' SKIP FOLDER: {url}  {text:8s}   {fol_absurl}       (of {folder_names})' )
             else:
                 if fol_absurl not in self.fetched and fol_absurl not in self.to_fetch_folders:
                     self.add_folder( fol_absurl )
@@ -180,12 +203,15 @@ class FRBRFetcher:
                 for _ in fetcher.work():
                     pass   # (you could access and print counters)
 
-            Note that there actually is no real distinction between what this class calls folders and pages,
-            but the way the repositories are structured, pretending this is true helps us go into what we added
-            as folders before what we added as pages, do that we act depth-first-like.
-            (and will e.g. start fetching documents before we've gone through all pages,
-            which seems like a good idea when some things have 100K pages, and there are reasons 
-            our fetching gets interrupted)
+            Note that there actually is no real distinction between 
+            what this class calls folders  and pages,
+            but the way the repositories are structured, pretending this is true helps us 
+            recurse into what we added as folders, 
+            before what we added as pages, 
+            so that we can act depth-first-like
+            and will e.g. start fetching documents before we've gone through all pages.
+            (which seems like a good idea when some things have 100K pages, 
+            and there are reasons our fetching gets interrupted)
 
             On a similar note, we cache things we consider folders, not things we consider pages 
             (because we assume many things at a level means it's the level (or at least a level)
@@ -219,6 +245,3 @@ class FRBRFetcher:
                 self.count_pages += 1
                 did_new_things = True
                 yield "LOOP" # dummy value
-
-
-
