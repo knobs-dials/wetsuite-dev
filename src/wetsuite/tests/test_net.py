62419fa6bd6c2b481444919217745e01d452b8a1
scarfboy <scarfboy@gmail.com>
2024-03-21 15:52:54 +0100

    broader test

--- src/wetsuite/tests/test_net.py
+++ src/wetsuite/tests/test_net.py
@@ -13,7 +13,7 @@ def test_download():
 
     download('https://www.example.com', show_progress=True)
 
-    with pytest.raises(ValueError, match=r'.*404.*'):
+    with pytest.raises(ValueError, match=r'.*(404|500).*'):
         download('https://www.example.com/noexist')
 
 
@@ -24,6 +24,6 @@ def test_download_to_file( tmp_path ): # note: pytest fixture will create a temp
     assert os.path.exists( tofile_path )
 
     # check that we don't create a file if we failed to start fetching
-    with pytest.raises(ValueError, match=r'.*404.*'):
+    with pytest.raises(ValueError, match=r'.*(404|500).*'):
         download('https://www.example.com/noexist', tofile_path=tofile_path)
         assert not os.path.exists( tofile_path )
